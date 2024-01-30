e0f3507db290c9a64822d018a5bada36cc1b6060
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-30 21:14:23 +0545

    reactserver

---
+++ controllers/blogController.js
@@ -0,0 +1,7 @@
+const add_blog = (req, res) => {};
+const update_blog = (req, res) => {};
+const delete_blog = (req, res) => {};
+const view_blogs = (req, res) => {};
+const view_blog = (req, res) => {};
+
+module.exports = { add_blog, update_blog, delete_blog, view_blog, view_blogs };
