f1a8a7d21e760953a9c83aacb1b3aab5359d3562
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-30 21:44:45 +0545

    server created

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,6 +6,6 @@ router.get("/blogs", authController.view_blogs);
 router.get("/blog/:id", authController.view_blog);
 router.post("/blog/add", authController.add_blog);
 router.post("/blog/update/:id", authController.update_blog);
-router.delete("/blog/delete/:id");
+router.delete("/blog/delete/:id",authController.delete_blog);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const blogRoutes = require("./routes/blogRoutes");
 
-
-app.listen(5000);
+app.use(cors({ origin: "http://localhost:3000" }));
+const dbURI =
+  "mongodb+srv://rudalkunwar:messi10@cluster0.frsepin.mongodb.net/nodeDB?retryWrites=true&w=majority";
+try {
+  app.listen(5000, () => {
+    console.log("Server Started");
+    mongoose
+      .connect(dbURI)
+      .then(() => {
+        console.log("Database Connected");
+      })
+      .catch((err) => {
+        console.log("Error Connecting to the database");
+      });
+  });
+} catch (error) {
+  console.log("Error connect to database");
+}
+app.use(blogRoutes);
