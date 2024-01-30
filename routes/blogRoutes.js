3d0bcb94d481324a282551c9526c8791d43e080f
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-30 21:21:05 +0545

    making routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,1 +1,11 @@
-import { Router } from "express";
+const Router = require("express");
+const authController = require("../controllers/blogController");
+const router = Router();
+
+router.get("/blogs", authController.view_blogs);
+router.get("/blog/:id", authController.view_blog);
+router.post("/blog/add", authController.add_blog);
+router.post("/blog/update/:id", authController.update_blog);
+router.delete("/blog/delete/:id");
+
+module.exports = router;
